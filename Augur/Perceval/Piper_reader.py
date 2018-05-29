3ede1b685b1853a41d58efad3c6364dc5187c15f
Keanu Nichols <nichols.keanu9@gmail.com>
2018-05-29 09:49:46 -0400

    Update Piper_reader.py

--- Augur/Perceval/Piper_reader.py
+++ Augur/Perceval/Piper_reader.py
@@ -5,16 +5,17 @@ import pprint
 import mysql.connector
 from sqlalchemy import create_engine
 import sqlalchemy as s
+from sqlalchemy_utils import database_exists, create_database
 #if(line[j:j+11]=="},\"unixfrom\"" or line[j:j+9] == "},\"origin\"" ):
 #9359610
-engine = s.create_engine('mysql+mysqlconnector://user:password@host_ip:port/database')
-
+#1355
+#Need to have pip install sqlalchemy-utils
 
 def read_json(p):
 	k = j = 0
 	y=""
 	for line in p:
-		#print(line)
+		#print(line,"\n\n")
 		if(p[j:j+9] == "\"origin\":" or p[j:j+11] == "\"unixfrom\":"):
 			k+=1
 			#print(p[temp:j])
@@ -26,6 +27,14 @@ def read_json(p):
 	return y,j
 
 def add_row(columns,df,di):
+	temp = 	di['data']['body']['plain']
+	words = ""
+	for j in range(0,len(temp)):
+		words+=temp[j]
+		if(temp[j] == "\n" and j+1<len(temp)):
+			if(temp[j+1] == ">"):
+				di['data']['body']['plain'] = words
+				break
 	li = [[di["backend_name"],di['category'],di['data']['Date'],
 		      di['data']['From'],di['data']['Message-ID'],
 		      di['data']['body']['plain']]]
@@ -34,42 +43,46 @@ def add_row(columns,df,di):
 	return df3
 	
 	
- 
-f = open('opendaylight-aaa-dev.json','r')
-x = f.read()
-temp = json.dumps(x)
-f.close()
-#print(y)
-data,j = read_json(x)
-#print(data,"\n\n")
-# decoding the JSON to dictionay
-di = json.loads(data)
-
-#print(di)
-# converting json dataset from dictionary to dataframe
-##print(di["data"]["body"]["plain"])
-#pprint.pprint(di)
-#Tried using Subject but sometimes they have fancy symbols that's
-#hard to upload to the database would have to decode it and upload
-#to the database and then encode it back when requesting from
-#the database
-columns = "backend_name","category","Date","From","Message-ID","Text"
-li = [[di["backend_name"],di['category'],di['data']['Date'],
-		      di['data']['From'],di['data']['Message-ID'],
-		      di['data']['body']['plain']]]
-df = pd.DataFrame(li,columns=columns)
-print(len(x))
-#print(j)
-while(j<len(x)):
-	data,r= read_json(x[j:])
-	j+=r
-	print(j,"\n\n\n")
-	if(j==len(x)):
-		break
+archives = ["aalldp-dev","aaa-dev"]
+engine = s.create_engine('mysql+mysqlconnector://root:Dc1Kk1Sh2Oh1@localhost/Pipermail?charset=utf8')
+if not database_exists(engine.url):
+    create_database(engine.url)
+for i in range(len(archives)):
+	f = open('opendaylight-' + archives[i] + '.json','r')
+	x = f.read()
+	temp = json.dumps(x)
+	f.close()
+	#print(y)
+	data,j = read_json(x)
+	#print(data,"\n\n")
+	# decoding the JSON to dictionay
 	di = json.loads(data)
-	df = add_row(columns,df,di)
 
-df = df.reset_index(drop=True)
-df.to_csv("temp.csv")
-print(pd.read_csv("temp.csv",index_col=0))
-df.to_sql(name='Pipermail', con=engine, if_exists = 'replace', index=False,)
+	#print(di)
+	# converting json dataset from dictionary to dataframe
+	##print(di["data"]["body"]["plain"])
+	#pprint.pprint(di)
+	#Tried using Subject but sometimes they have fancy symbols that's
+	#hard to upload to the database would have to decode it and upload
+	#to the database and then encode it back when requesting from
+	#the database
+	columns = "backend_name","category","Date","From","Message-ID","Text"
+	li = [[di["backend_name"],di['category'],di['data']['Date'],
+			      di['data']['From'],di['data']['Message-ID'],
+			      di['data']['body']['plain']]]
+	df = pd.DataFrame(li,columns=columns)
+	print(len(x))
+	#print(j)
+	while(j<len(x)):
+		data,r= read_json(x[j:])
+		j+=r
+		print(j,"\n\n\n")
+		if(j==len(x)):
+			break
+		di = json.loads(data)
+		df = add_row(columns,df,di)
+
+	df = df.reset_index(drop=True)
+	df.to_csv(archives[i] + ".csv")
+	print(pd.read_csv(archives[i] + ".csv",index_col=0))
+	df.to_sql(name="temp"+str(i), con=engine, if_exists = 'replace', index=False,)
